6c4ef8af029a49cb0ae65af1b9b84cbdda1763a2
NiaPayton <shadeanpayton@gmail.com>
2022-10-29 01:55:05 -0500

    Added completed project 1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+
+window.onload = function(){
+    let message = document.getElementsByClassName('message')[0]
+    let btn = document.getElementsByTagName('button')[0]
+    let mail = document.getElementsByTagName('input')[0]
+    let is_valid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+    btn.addEventListener('click',  function(e){
+        e.preventDefault()
+        let mail_value = mail.value
+        if (mail_value == "" || !mail_value.match(is_valid)){
+            message.innerHTML = 'Please enter avalid email address.'
+            mail.value = ''
+        }
+        else if (mail_value != "" && mail_value.match(is_valid)){
+            message.innerHTML = `Thank You! Your email address ${mail_value} has been added to our mailing list!`
+            mail.value = ''
+        }
+
+    })
+
+}
+
